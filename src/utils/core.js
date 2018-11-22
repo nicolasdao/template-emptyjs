e7ddfbed3955773471a4b43790481df606e04b72
Nicolas Dao <nicolas.dao@gmail.com>
2018-11-22 12:44:40 +1100

    feat: Add set of common utilities

---
+++ src/utils/core.js
@@ -0,0 +1,481 @@
+/**
+ * Copyright (c) 2017-2018, Neap Pty Ltd.
+ * All rights reserved.
+ * 
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+*/
+
+const shortid = require('shortid')
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////                           START COLLECTION                           ////////////////////////////////
+
+/**
+ * Breaks down an array into an array with 2 items:
+ * 	[0]: Head of size 'headSize' (default 'headSize' is 1)
+ * 	[1]: The rest of the items
+ * 	
+ * @param  {Array}   a        	Original array
+ * @param  {Number} headSize 	Default 1
+ * @return {Array}           	Array of length 2
+ */
+const headTail = (a, headSize=1) => (a || []).reduce((acc, v, idx) => {
+	idx < headSize ? acc[0].push(v) : acc[1].push(v)
+	return acc
+}, [[],[]])
+
+
+const uniq = (a, fn) => {
+	fn = fn || (x => x)
+	return Object.keys((a || []).reduce((acc,x) => {
+		const v = fn(x)
+		if (!acc[v])
+			acc[v] = true
+		return acc
+	}, {}))
+}
+
+const _objectSortBy = (obj, fn = x => x, dir='asc') => Object.keys(obj || {})
+	.map(key => ({ key, value: obj[key] }))
+	.sort((a,b) => {
+		const vA = fn(a.value)
+		const vB = fn(b.value)
+		if (dir == 'asc') {
+			if (vA < vB)
+				return -1
+			else if (vA > vB)
+				return 1
+			else
+				return 0
+		} else {
+			if (vA > vB)
+				return -1
+			else if (vA < vB)
+				return 1
+			else
+				return 0
+		}
+	}).reduce((acc,v) => {
+		acc[v.key] = v.value
+		return acc
+	}, {})
+
+const _arraySortBy = (arr, fn = x => x, dir='asc') => (arr || []).sort((a,b) => {
+	const vA = fn(a)
+	const vB = fn(b)
+	if (dir == 'asc') {
+		if (vA < vB)
+			return -1
+		else if (vA > vB)
+			return 1
+		else
+			return 0
+	} else {
+		if (vA > vB)
+			return -1
+		else if (vA < vB)
+			return 1
+		else
+			return 0
+	}
+})
+
+const sortBy = (obj, fn = x => x, dir='asc') => Array.isArray(obj) ? _arraySortBy(obj, fn, dir) : _objectSortBy(obj, fn, dir)
+const newSeed = (size=0) => Array.apply(null, Array(size))
+
+const mergeCollection = (...collections) => {
+	if (collections.length == 0)
+		return []
+
+	const lengths = collections.filter(col => col && col.length).map(col => col.length)
+	if (lengths.length == 0)
+		return collections
+	
+	const maxLength = Math.max(...collections.filter(col => col && col.length).map(col => col.length))
+
+	return collections.map(col => {
+		const l = (col || []).length
+		if (l == 0) {
+			return newSeed(maxLength)
+		}
+		if (l == maxLength)
+			return col 
+
+		const diff = maxLength - l
+		return [...col, ...newSeed(diff)]
+	})
+}
+
+//////////////////////////                           END COLLECTION                             ////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////                           START CONVERTER                           	////////////////////////////////
+
+/**
+ * Convert snake case to camel case
+ * @param  {String} s 	e.g., "hello_world"
+ * @return {String}   	e.g., "helloWorld"
+ */
+const s2cCase = s => (s || '').replace(/\s/g, '').toLowerCase().split(/(?=_.{1})/g).reduce((result, part) => result + (result ? (part.slice(1,2).toUpperCase() + part.slice(2)) : part), '')
+
+/**
+ * Convert camel case to snake case
+ * @param  {String} s 	e.g., "helloWorld"
+ * @return {String}   	e.g., "hello_world"
+ */
+const c2sCase = s => (s || '').replace(/\s/g, '').split(/(?=[A-Z]{1})/g).map(x => x.toLowerCase()).join('_')
+
+// Transforms { helloWorld: 'Nic' } to { hello_world: 'Nic' }
+const objectC2Scase = obj => {
+	if (!obj || typeof(obj) != 'object') 
+		return obj 
+
+	return Object.keys(obj).reduce((acc, key) => {
+		const v = obj[key]
+		const p = c2sCase(key)
+		if (v && typeof(v) == 'object') {
+			if (Array.isArray(v))
+				acc[p] = v.map(x => objectC2Scase(x))
+			else
+				acc[p] = objectC2Scase(v)
+		} else
+			acc[p] = v 
+		return acc
+	}, {})
+}
+
+// Transforms { hello_world: 'Nic' } to { helloWorld: 'Nic' }
+const objectS2Ccase = obj => {
+	if (!obj || typeof(obj) != 'object') 
+		return obj 
+
+	return Object.keys(obj).reduce((acc, key) => {
+		const v = obj[key]
+		const p = s2cCase(key)
+		if (v && typeof(v) == 'object') {
+			if (Array.isArray(v))
+				acc[p] = v.map(x => objectS2Ccase(x))
+			else
+				acc[p] = objectS2Ccase(v)
+		} else
+			acc[p] = v 
+		return acc
+	}, {})
+}
+
+//////////////////////////                           END CONVERTER	                            ////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////                           END DATETIME HELPER                        ////////////////////////////////
+
+const getDateUtc = (date) => {
+	const now_utc =  Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds())
+	return new Date(now_utc)
+}
+
+const addZero = nbr => ('0' + nbr).slice(-2)
+
+const getTimestamp = (options={ short:true }) => {
+	const d = getDateUtc(new Date())
+	const main = `${d.getUTCFullYear()}${addZero(d.getUTCMonth()+1)}${addZero(d.getUTCDate())}`
+	if (options.short)
+		return main
+	else 
+		return `${main}-${addZero(d.getUTCHours())}${addZero(d.getUTCMinutes())}${addZero(d.getUTCSeconds())}`
+}
+
+const addDaysToDate = (d, v=0) => {
+	const t = new Date(d)
+	t.setDate(d.getDate() + v)
+	return t
+}
+
+const addMonthsToDate = (d, v=0) => {
+	const t = new Date(d)
+	t.setMonth(d.getMonth() + v)
+	return t
+}
+
+const addYearsToDate = (d, v=0) => {
+	const t = new Date(d)
+	t.setYear(d.getFullYear() + v)
+	return t
+}
+
+const addHoursToDate = (d, v=0) => {
+	const t = new Date(d)
+	t.setHours(d.getHours() + v)
+	return t
+}
+
+const addMinutesToDate = (d, v=0) => {
+	const t = new Date(d)
+	t.setMinutes(d.getMinutes() + v)
+	return t
+}
+
+const addSecondsToDate = (d, v=0) => {
+	const t = new Date(d)
+	t.setSeconds(d.getSeconds() + v)
+	return t
+}
+
+//////////////////////////                           END DATETIME HELPER                        ////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////                           START IDENTITY                             ////////////////////////////////
+
+const newId = (options={}) => {
+	const id = shortid.generate().replace(/-/g, 'r').replace(/_/g, '9')
+	return options.short ? id.slice(0,-4) : id
+
+}
+
+//////////////////////////                           START IDENTITY                             ////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////                             START MATH                                ///////////////////////////////
+
+/**
+ * Calculates the average of a specific field on an array objects
+ * 
+ * @param  {Array}   	arr 	e.g. [{ name: 'Nic', age: 36 }, { name: 'Boris', age: 30 }]
+ * @param  {Function} 	fn  	e.g. x => x.age
+ * @return {Number}       		e.g. 33
+ */
+const avg = (arr=[], fn) => {
+	if (arr.length == 0) 
+		return null 
+	const f = fn || (x => x) 
+	return arr.reduce((a,v) => a + f(v), 0)/arr.length
+}
+
+/**
+ * Calculates the standard deviation of a specific field on an array objects
+ * 
+ * @param  {Array}   	arr 	e.g. [{ name: 'Nic', age: 36 }, { name: 'Boris', age: 30 }]
+ * @param  {Function} 	fn  	e.g. x => x.age
+ * @return {Number}       		e.g. 33
+ */
+const stdDev = (arr=[], fn) => {
+	if (arr.length == 0) 
+		return null
+	const f = fn || (x => x)
+	const { result, resultSquare } = arr.reduce((a,v) => {
+		const val = f(v)
+		return { result: a.result + val, resultSquare: a.resultSquare + Math.pow(val,2) }
+	}, { result:0, resultSquare:0 })
+
+	const l = arr.length
+	return Math.sqrt((resultSquare/l) - Math.pow((result/l), 2))
+}
+
+/**
+ * Calculates the median deviation of a specific field on an array objects
+ * 
+ * @param  {Array}   	arr 	e.g. [{ name: 'Nic', age: 36 }, { name: 'Boris', age: 30 }]
+ * @param  {Function} 	fn  	e.g. x => x.age
+ * @return {Number}       		e.g. 33
+ */
+const median = (arr=[], fn) => {
+	const f = fn || (x => x)
+	const l = arr.length
+	if (l == 0)
+		return null
+	// odd length
+	else if (l & 1) 
+		return arr.map(x => f(x)).sort((a,b) => a >= b)[Math.floor(l/2)]
+	// even length
+	else {
+		const idx_1 = Math.floor(l/2)
+		const idx_0 = idx_1 - 1
+		const a = arr.map(x => f(x)).sort((a,b) => a >= b)
+		return (a[idx_0] + a[idx_1])/2
+	}
+}
+
+const getRandomNumber = (start, end) => {
+	const size = end == undefined ? start : (end - start)
+	const offset = end == undefined ? 0 : start
+	return offset + Math.floor(Math.random() * size)
+}
+
+//////////////////////////                              END MATH                                 ///////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////                         START OBJECT HELPERS                                 ////////////////////////
+
+const mergeObj = (...objs) => objs.reduce((acc, obj) => { //Object.assign(...objs.map(obj => JSON.parse(JSON.stringify(obj))))
+	obj = obj || {}
+	if (typeof(obj) != 'object' || Array.isArray(obj) || (obj instanceof Date))
+		return acc
+	
+	Object.keys(obj).forEach(property => {
+		const val = obj[property]
+		const originVal = acc[property]
+		const readyToMerge = !originVal || !val || typeof(val) != 'object' || Array.isArray(val) || typeof(originVal) != 'object' || Array.isArray(originVal)
+		acc[property] = readyToMerge ? val : mergeObj(originVal, val)	
+	})
+
+	return acc
+}, {})
+
+const isEmptyObj = obj => {
+	if (!obj)
+		return true 
+	try {
+		const o = JSON.stringify(obj)
+		return o == '{}'
+	} catch(e) {
+		return (() => false)(e)
+	}
+}
+
+const isObj = obj => {
+	if (!obj || typeof(obj) != 'object')
+		return false 
+
+	try {
+		const o = JSON.stringify(obj) || ''
+		return o.match(/^\{(.*?)\}$/)
+	} catch(e) {
+		return (() => false)(e)
+	}
+}
+
+const getDiff = (orig={}, current={}) => {
+	return Object.keys(current).reduce((acc, key) => {
+		const val = current[key]
+		const origVal = orig[key]
+		if (val == undefined || origVal == val) 
+			return acc
+		
+		const origValIsObj = isObj(origVal)
+
+		if (!origValIsObj && origVal != val) {
+			acc[key] = val
+			return acc
+		} 
+
+		const valIsObj = isObj(val)
+
+		if (origValIsObj && valIsObj) {
+			const objDiff = getDiff(origVal, val)
+			if (!isEmptyObj(objDiff))
+				acc[key] = objDiff
+			return acc
+		}
+
+		if (origVal != val) {
+			acc[key] = val
+			return acc
+		} 
+		return acc
+	}, {})
+}
+
+const objAreSame = (obj1, obj2) => {
+	const o = getDiff(obj1, obj2)
+	return Object.keys(o || {}).length == 0
+}
+
+const arrayObjAreDiff = (objArr_01, objArr_02) => {
+	objArr_01 = objArr_01 || []
+	objArr_02 = objArr_02 || []
+	if (objArr_01.length != objArr_02.length)
+		return false 
+	return objArr_01.some(h1 => !objArr_02.some(h2 => objAreSame(h1, h2)))
+}
+
+//////////////////////////                         START OBJECT HELPERS                                 ////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////                         START VALIDATE HELPERS                               ////////////////////////
+
+const validateUrl = (value='') => /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value)
+
+const validateEmail = (value='') => /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i.test(value)
+
+const validateIsDate = (d, options={ exception: { toggle: false, message: null } }) => { 
+	const test = d && typeof(d.getTime) == 'function' && !isNaN(d.getTime())
+	if (!test && options.exception.toggle)
+		throw new Error(options.exception.message || `${d} is not a Date object.`)
+	return test
+}
+
+//////////////////////////                          END VALIDATE HELPERS                                ////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+module.exports = {
+	collection: {
+		uniq,
+		headTail,
+		sortBy,
+		seed: newSeed,
+		merge: mergeCollection
+	},
+	converter: {
+		s2cCase,
+		c2sCase,
+		objectC2Scase,
+		objectS2Ccase
+	},
+	date: {
+		timestamp: getTimestamp,
+		addDays: addDaysToDate,
+		addMonths: addMonthsToDate,
+		addYears: addYearsToDate,
+		addHours: addHoursToDate,
+		addMinutes: addMinutesToDate,
+		addSeconds: addSecondsToDate
+	},
+	identity: {
+		'new': newId
+	},
+	math: {
+		avg,
+		stdDev,
+		median,
+		randomNumber: getRandomNumber
+	},
+	obj: {
+		merge: mergeObj,
+		isEmpty: isEmptyObj,
+		isObj,
+		diff: getDiff,
+		same: objAreSame,
+		arrayAreDiff: arrayObjAreDiff
+	},
+	validate: {
+		url: validateUrl,
+		date: validateIsDate,
+		email: validateEmail
+	}
+}
---
+++ src/utils/promise.js
@@ -0,0 +1,170 @@
+/**
+ * Copyright (c) 2017-2018, Neap Pty Ltd.
+ * All rights reserved.
+ * 
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+*/
+
+const { obj: { merge }, identity, math } = require('./core')
+const { arities } = require('./functional')
+
+const delay = timeout => new Promise(onSuccess => setTimeout(onSuccess, timeout))
+
+const wait = (stopWaiting, options) => Promise.resolve(null).then(() => {
+	const now = Date.now()
+	const { timeout=300000, start=now, interval=2000 } = options || {}
+	
+	if ((now - start) > timeout)
+		throw new Error('timeout')
+	
+	return Promise.resolve(null).then(() => stopWaiting()).then(stop => {
+		if (stop)
+			return
+		else
+			return delay(interval).then(() => wait(stopWaiting, { timeout, start, interval }))
+	})
+})
+
+/**
+ * Add 3 new functions on a promise. Each one return a boolean:
+ * 	1. isFulfilled()
+ * 	2. isPending()
+ * 	3. isRejected()
+ * 	
+ * @param  {Promise} promise Original promise
+ * @return {Promise}         Same promise as the input, augmented with 3 new functions
+ */
+const makePromiseQueryable = promise => {
+	// Don't modify any promise that has been already modified.
+	if (promise.isResolved) return promise
+
+	// Set initial state
+	let isPending = true
+	let isRejected = false
+	let isFulfilled = false
+
+	// Observe the promise, saving the fulfillment in a closure scope.
+	let result = promise.then(
+		v => {
+			isFulfilled = true
+			isPending = false
+			return v
+		}, 
+		e => {
+			isRejected = true
+			isPending = false
+			throw e
+		}
+	)
+
+	result.isFulfilled = () => isFulfilled
+	result.isPending = () => isPending
+	result.isRejected = () => isRejected
+	return result
+}
+
+/**
+ * Makes a promise throw an error if it times our
+ * @param  {Promise} p       	Original promise
+ * @param  {Number} timeOut 	Optional. Default is 30,000 milliseconds
+ * @return {Promise}         	[description]
+ */
+const addTimeout = (p, timeOut=30000) => {
+	const timeoutMsg = `timout_${identity.new()}`
+	const timeoutTask = new Promise(onSuccess => setTimeout(() => onSuccess(timeoutMsg), timeOut))
+	return Promise.race([timeoutTask, p])
+	.then(res => {
+		if (res == timeoutMsg)
+			throw new Error(`timeout`)
+		return res
+	})
+}
+
+/**
+ * [description]
+ * @param  {Function} fn        				[description]
+ * @param  {Function} successFn 				(res, options) => Returns a promise or a value. The value is a boolean or an object that determines 
+ *                                  			whether a response is valid or not. If the value is an object, that object might contain
+ *                                  			a 'retryInterval' which overrides the optional value. 
+ * @param  {Function} failureFn 				(Optional) (error, options) => Returns a promise or a value. The value is a boolean or an object that determines 
+ *                                  			whether a response is valid or not. If the value is an object, that object might contain
+ *                                  			a 'retryInterval' which overrides the optional value.                          			
+ * @param  {Number}   options.retryAttempts   	default: 5. Number of retry
+ * @param  {Number}   options.attemptsCount   	Current retry count. When that counter reaches the 'retryAttempts', the function stops.
+ * @param  {Number}   options.timeOut   		If specified, 'retryAttempts' and 'attemptsCount' are ignored
+ * @param  {Number}   options.retryInterval   	default: 5000. Time interval in milliseconds between each retry. It can also be a 2 items array.
+ *                                             	In that case, the retryInterval is a random number between the 2 ranges (e.g., [10, 100] => 54)
+ * @param  {Boolean}  options.ignoreError   	In case of constant failure to pass the 'successFn' test, this function will either throw an error
+ *                                           	or return the current result without throwing an error if this flag is set to true.
+ * @param  {String}   options.errorMsg   		Customize the exception message in case of failure.
+ * @param  {String}   options.ignoreFailure   	If set to true, then failure from fn will cause a retry
+ * @return {[type]}             				[description]
+ */
+const retry = arities(
+	'function fn, function successFn, object options={}',
+	'function fn, function successFn, function failureFn, object options={}',
+	({ fn, successFn, failureFn, options={} }) => { 
+		const start = Date.now()
+		return Promise.resolve(null)
+			.then(() => fn()).then(data => ({ error: null, data }))
+			.catch(error => { 
+				if (options.ignoreFailure && !failureFn)
+					failureFn = () => true
+				return { error, data: null }
+			})
+			.then(({ error, data }) => Promise.resolve(null)
+				.then(() => {
+					if (error && failureFn)
+						return failureFn(error, options)
+					else if (error)
+						throw error 
+					else
+						return successFn(data, options)
+				})
+				.then(passed => {
+					if (!error && passed)
+						return data
+					else if ((!error && !passed) || (error && passed)) {
+						let { retryAttempts=5, retryInterval=5000, attemptsCount=0, timeOut=null, startTime=null } = options
+						if (timeOut > 0) {
+							startTime = startTime || start
+							if (Date.now() - startTime < timeOut) {
+								const explicitRetryInterval = passed && passed.retryInterval > 0 ? passed.retryInterval : null
+								const i = (!explicitRetryInterval && Array.isArray(retryInterval) && retryInterval.length > 1)
+									? (() => {
+										if (typeof(retryInterval[0]) != 'number' || typeof(retryInterval[1]) != 'number')
+											throw new Error(`Wrong argument exception. When 'options.retryInterval' is an array, all elements must be numbers. Current: [${retryInterval.join(', ')}].`)
+										if (retryInterval[0] > retryInterval[1])
+											throw new Error(`Wrong argument exception. When 'options.retryInterval' is an array, the first element must be strictly greater than the second. Current: [${retryInterval.join(', ')}].`)
+
+										return math.randomNumber(retryInterval[0], retryInterval[1])
+									})()
+									: (explicitRetryInterval || retryInterval)
+
+								return delay(i).then(() => failureFn 
+									? retry(fn, successFn, failureFn, merge(options, { startTime }))
+									: retry(fn, successFn, merge(options, { startTime })))
+							} else
+								throw new Error('timeout')
+						} else if (attemptsCount < retryAttempts)
+							return delay(retryInterval).then(() => failureFn
+								? retry(fn, successFn, failureFn, merge(options, { attemptsCount:attemptsCount+1 }))
+								: retry(fn, successFn, merge(options, { attemptsCount:attemptsCount+1 })))
+						else if (options.ignoreError)
+							return data
+						else 
+							throw new Error(options.errorMsg ? options.errorMsg : `${retryAttempts} attempts to retry the procedure failed to pass the test`)
+					} else 
+						throw error
+				}))
+	})
+
+
+module.exports = {
+	delay,
+	wait,
+	retry,
+	makePromiseQueryable,
+	addTimeout
+}

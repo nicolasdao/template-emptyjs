f4d500fef8164955c0cfb10ca22a30b2510f4867
Nicolas Dao <nicolas.dao@gmail.com>
2018-11-26 14:05:40 +1100

    Update fetch.js

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -6,26 +6,35 @@
  * LICENSE file in the root directory of this source tree.
 */
 const fetch = require('node-fetch')
+const { getInfo } = require('./url')
 
-const _processResponse = (res, options={}) => (!options.format || options.format == 'json' ? res.json() : res.text())
-	.then(data => ({ status: res.status, data }))
-	.catch(() => ({ status: 200, data: res }))
+const _processResponse = (res, uri) => {
+	const { ext, contentType } = getInfo(uri || '')
+	const action = !ext || !contentType || !contentType.match(/(html|css|xml|javascript|rss|csv)/) ? (() => res.json()) : (() => res.text())
+	return action()
+		.then(data => ({ status: res.status, data }))
+		.catch(() => ({ status: 200, data: res }))
+}
 
-const postData = (url, headers={}, body, options={}) => 
-	fetch(url, { method: 'POST', headers, body }).then(res => _processResponse(res, options))
+const postData = (url, headers={}, body) => 
+	fetch(url, { method: 'POST', headers, body }).then(res => _processResponse(res, url))
 
-const getData = (url, headers={}, options={}) => 
-	fetch(url, { method: 'GET', headers }).then(res => _processResponse(res, options))
+const putData = (url, headers={}, body) => 
+	fetch(url, { method: 'PUT', headers, body }).then(res => _processResponse(res, url))
 
-const deleteData = (url, headers={}, body, options={}) => 
-	fetch(url, { method: 'DELETE', headers, body }).then(res => _processResponse(res, options))
+const patchData = (url, headers={}, body) => 
+	fetch(url, { method: 'PATCH', headers, body }).then(res => _processResponse(res, url))
 
-const putData = (url, headers={}, body, options={}) => 
-	fetch(url, { method: 'PUT', headers, body }).then(res => _processResponse(res, options))
+const deleteData = (url, headers={}, body) => 
+	fetch(url, { method: 'DELETE', headers, body }).then(res => _processResponse(res, url))
+
+const getData = (url, headers={}) => 
+	fetch(url, { method: 'GET', headers }).then(res => _processResponse(res, url))
 
 module.exports = {
-	'get': getData,
 	post: postData,
+	'get': getData,
 	put: putData,
+	patch: patchData,
 	delete: deleteData
 }

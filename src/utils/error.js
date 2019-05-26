2711f0f70b4e84f5a1cf5538b4ae883db5a100ca
Nicolas Dao <nicolas.dao@gmail.com>
2019-05-26 12:51:29 +1000

    Update error.js

--- src/utils/error.js
+++ src/utils/error.js
@@ -6,100 +6,124 @@
  * LICENSE file in the root directory of this source tree.
 */
 
-const throwIfUndefined = (value, valueName) => {
+const { validate } = require('./core')
+
+const _throwError = (message, extraData) => {
+	extraData = extraData || {}
+	if (!extraData || typeof(extraData) != 'object' || Object.keys(extraData).length == 0)
+		throw new Error(message || '')
+
+	let e = new Error(message || '')
+	Object.assign(e, extraData)
+	throw e
+}
+
+const throwIfInvalidEmail = (email, valueName, extraData) => {
+	if (!validate.email(email))
+		_throwError(`Invalid email${valueName ? ` '${valueName}'` : ''}.`, extraData)
+	return email
+}
+
+const throwIfInvalidURL = (url, valueName, extraData) => {
+	if (!validate.url(url))
+		_throwError(`Invalid URL${valueName ? ` '${valueName}'` : ''}.`, extraData)
+	return url
+}
+
+const throwIfUndefined = (value, valueName, extraData) => {
 	if (value === undefined)
-		throw new Error(`Missing required argument${valueName ? ` '${valueName}'` : ''}.`)
+		_throwError(`Missing required argument${valueName ? ` '${valueName}'` : ''}.`, extraData)
 	return value
 }
 
-const throwIfNotTruthy = (value, valueName) => {
+const throwIfNotTruthy = (value, valueName, extraData) => {
 	if (!value)
-		throw new Error(`Missing required argument${valueName ? ` '${valueName}'` : ''}.`)
+		_throwError(`Missing required argument${valueName ? ` '${valueName}'` : ''}.`, extraData)
 	return value
 }
 
-const throwIfNotNumber = (value, valueName) => {
+const throwIfNotNumber = (value, valueName, extraData) => {
 	const t = typeof(value)
 	if (t != 'number')
-		throw new Error(`Wrong argument exception. ${valueName ? ` '${valueName}'` : 'The value'} must be a number (current: ${t}).`)
+		_throwError(`Wrong argument exception. ${valueName ? ` '${valueName}'` : 'The value'} must be a number (current: ${t}).`, extraData)
 	return value
 }
 
 // e.g., throwIfWrongValue(type, 'type', ['YEAR', 'MONTH'])
-const throwIfWrongValue = (value, valueName, validValues) => {
+const throwIfWrongValue = (value, valueName, validValues, extraData) => {
 	if (!value)
-		throw new Error('Failed to test value against a list of valid value. No value was passed.')
+		_throwError('Failed to test value against a list of valid value. No value was passed.', extraData)
 	if (!valueName)
-		throw new Error('Failed to test value against a list of valid value. Missing second argument \'valueName\'.')
+		_throwError('Failed to test value against a list of valid value. Missing second argument \'valueName\'.', extraData)
 	if (typeof(valueName) != 'string')
-		throw new Error('Failed to test value against a list of valid value. Wrong argument exception. The second argument \'valueName\' must be a string.')
+		_throwError('Failed to test value against a list of valid value. Wrong argument exception. The second argument \'valueName\' must be a string.', extraData)
 	if (!validValues)
-		throw new Error('Failed to test value against a list of valid value. Missing third required argument \'validValues\'.')
+		_throwError('Failed to test value against a list of valid value. Missing third required argument \'validValues\'.', extraData)
 
 	const valid = Array.isArray(validValues) ? validValues.some(v => v == value) : value == validValues
 	if (valid)
 		return value
 	else
-		throw new Error(`Value for variable '${valueName}' is invalid. Valid values are ${validValues} (current: ${value}).`)
+		_throwError(`Value for variable '${valueName}' is invalid. Valid values are ${validValues} (current: ${value}).`, extraData)
 }
 
-const throwIfNoMatch = (value, valueName, regex) => {
+const throwIfNoMatch = (value, valueName, regex, extraData) => {
 	if (!value)
-		throw new Error('Failed to test value against a regex. No value was passed.')
+		_throwError('Failed to test value against a regex. No value was passed.', extraData)
 	if (!valueName)
-		throw new Error('Failed to test value against a regex. Missing second argument \'valueName\'.')
+		_throwError('Failed to test value against a regex. Missing second argument \'valueName\'.', extraData)
 	if (typeof(valueName) != 'string')
-		throw new Error('Failed to test value against a regex. Wrong argument exception. The second argument \'valueName\' must be a string.')
+		_throwError('Failed to test value against a regex. Wrong argument exception. The second argument \'valueName\' must be a string.', extraData)
 	if (!regex)
-		throw new Error('Failed to test value against a regex. Missing third required argument \'regex\'.')
+		_throwError('Failed to test value against a regex. Missing third required argument \'regex\'.', extraData)
 	if (!(regex instanceof RegExp))
-		throw new Error('Failed to test value against a regex. Third required argument \'regex\' is not a RegExp.')
+		_throwError('Failed to test value against a regex. Third required argument \'regex\' is not a RegExp.', extraData)
 
 	const valid = regex.test(value)
 	if (valid)
 		return value
 	else
-		throw new Error(`Value for variable '${valueName}' is invalid. It does not match regex ${regex}.`)
+		_throwError(`Value for variable '${valueName}' is invalid. It does not match regex ${regex}.`, extraData)
 }
 
 // e.g., throwIfGreaterThan(startDate, endDate, 'startDate', 'endDate')
-const throwIfGreaterThan = (value1, value2, valueName1, valueName2) => {
+const throwIfGreaterThan = (value1, value2, valueName1, valueName2, extraData) => {
 	if (!value1)
-		throw new Error('Failed to compare value1 with value2. No value1 was passed.')
+		_throwError('Failed to compare value1 with value2. No value1 was passed.', extraData)
 	if (!value2)
-		throw new Error('Failed to compare value1 with value2. No value2 was passed.')
+		_throwError('Failed to compare value1 with value2. No value2 was passed.', extraData)
 
 	const valid = value1 <= value2
 	if (valid)
 		return [value1, value2]
 	else {
 		if (valueName1 && valueName2)
-			throw new Error(`'${valueName1}' must be smaller or equal to '${valueName2}' (current: ${value1}(${valueName1}) > ${value2}(${valueName2})).`)
+			_throwError(`'${valueName1}' must be smaller or equal to '${valueName2}' (current: ${value1}(${valueName1}) > ${value2}(${valueName2})).`, extraData)
 		else
-			throw new Error(`'value1' must be smaller or equal to 'value2' (current: ${value1}(value1) > ${value2}(value2)).`)
+			_throwError(`'value1' must be smaller or equal to 'value2' (current: ${value1}(value1) > ${value2}(value2)).`, extraData)
 	}
 }
 
 // e.g., throwIfNotBetween(age, 'age', [18, 65])
-const throwIfNotBetween = (value, valueName, validValues) => {
+const throwIfNotBetween = (value, valueName, validValues, extraData) => {
 	if (!value)
-		throw new Error('Failed to test value against a list of valid value. No value was passed.')
+		_throwError('Failed to test value against a list of valid value. No value was passed.', extraData)
 	if (!valueName)
-		throw new Error('Failed to test value against a list of valid value. Missing second argument \'valueName\'.')
+		_throwError('Failed to test value against a list of valid value. Missing second argument \'valueName\'.', extraData)
 	if (typeof(valueName) != 'string')
-		throw new Error('Failed to test value against a list of valid value. Wrong argument exception. The second argument \'valueName\' must be a string.')
+		_throwError('Failed to test value against a list of valid value. Wrong argument exception. The second argument \'valueName\' must be a string.', extraData)
 	if (!validValues)
-		throw new Error('Failed to test value against a list of valid value. Missing third required argument \'validValues\'.')
+		_throwError('Failed to test value against a list of valid value. Missing third required argument \'validValues\'.', extraData)
 	if (validValues.length != 2)
-		throw new Error('Failed to test value against a list of valid value. 3rd argument \'validValues\' must be an array with 2 values.')
+		_throwError('Failed to test value against a list of valid value. 3rd argument \'validValues\' must be an array with 2 values.', extraData)
 	if (validValues[0] > validValues[1])
-		throw new Error('Failed to test value against a list of valid value. 1st element of the \'validValues\' array must be smaller or equal to the 2nd element.')
+		_throwError('Failed to test value against a list of valid value. 1st element of the \'validValues\' array must be smaller or equal to the 2nd element.', extraData)
 
 	const valid = validValues[0] <= value && value <= validValues[1]
 	if (valid)
 		return value
 	else
-		throw new Error(`Value for variable '${valueName}' is invalid. ${value} is not between ${validValues[0]} and ${validValues[1]}`)
+		_throwError(`Value for variable '${valueName}' is invalid. ${value} is not between ${validValues[0]} and ${validValues[1]}`, extraData)
 }
 
 module.exports = {
@@ -109,6 +133,8 @@ module.exports = {
 	throwIfWrongValue,
 	throwIfNoMatch,
 	throwIfGreaterThan,
-	throwIfNotBetween
+	throwIfNotBetween,
+	throwIfInvalidURL,
+	throwIfInvalidEmail
 }
 
